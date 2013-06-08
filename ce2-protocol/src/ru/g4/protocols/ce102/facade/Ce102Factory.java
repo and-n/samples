090458a767e1a68be827508f0c8468c1ba3e8344
qw3rtrun <qw3rtrun@qw3rtrun-ubuntu>
2013-06-08 17:28:30 +0400

    Простая реализация Ce102Factory

--- ce2-protocol/src/ru/g4/protocols/ce102/facade/Ce102Factory.java
+++ ce2-protocol/src/ru/g4/protocols/ce102/facade/Ce102Factory.java
@@ -1,6 +1,32 @@
 package ru.g4.protocols.ce102.facade;
 
-public class Ce102Factory {
+import java.util.TimeZone;
 
+import ru.g4.phch2.factory.ChannelParameters;
+
+public class Ce102Factory {
 	
+	private long timeout = 1000;
+	private TimeZone tz = TimeZone.getDefault();
+	
+	public Ce102Factory(long timeout, TimeZone tz) {
+		super();
+		this.timeout = timeout;
+		this.tz = tz;
+	}
+	
+	public Ce102Factory(TimeZone tz) {
+		super();
+		this.tz = tz;
+	}
+	
+	public Ce102Factory(long timeout) {
+		super();
+		this.timeout = timeout;
+	}
+
+	public Ce102 getCE102(final ChannelParameters<?> params, final int address, final String password) 
+	{
+		return new Ce102Impl(params, address, password, tz, timeout);
+	}
 }
